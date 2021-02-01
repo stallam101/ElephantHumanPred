916a08f052535abcfda89995cb09bd116587a9e2
Sahil Tallam <sahiltallam@Kanwars-MacBook-Air-6608.local>
2021-01-31 21:17:42 -0800

    added contact page

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
+
 if __name__ == "__main__":
     app.run()
